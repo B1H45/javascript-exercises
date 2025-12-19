ee8d8e8eb295092e5b5fcecb84c5574e3f18e930
Ben Hustler <ben1hustler@gmail.com>
2025-12-19 01:51:57 +0000

    DOM exercises

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split("");
+    let revArr = arr.reverse();
+    let returnStr = revArr.join('');
+    return returnStr;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...rems) {
+    for (let item of rems) {
+        arr = arr.filter(element => element !== item);
+    }
+    console.log(`\n ${arr} \n`);
 
+    return arr;
+}
 // Do not edit below this line
 module.exports = removeFromArray;

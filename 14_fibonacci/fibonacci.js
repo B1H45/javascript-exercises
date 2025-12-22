179571f49b1ffc8d43f2c64574b0d33c3383ada9
Ben Hustler <ben1hustler@gmail.com>
2025-12-22 00:27:27 +0000

    Stuff

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let back2 = 1;
+    let back1 = 1;
+    let temp = 1;
 
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    if (num == 0) {
+        return 0;
+    }
+    if (num == 1) {
+        return 1;
+    }
+
+    for (let i = 2; i < num; i ++) {
+        temp = back1 + back2;
+        back2 = back1;
+        back1 = temp;
+    }
+
+    return temp;
 };
 
 // Do not edit below this line

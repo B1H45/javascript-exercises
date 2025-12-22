15855d34c465663606b4259fc9f4c6fcedb09a27
Ben Hustler <ben1hustler@gmail.com>
2025-12-22 04:48:02 +0000

    Things

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b; 
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (item of arr) {
+    sum +=item;
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (item of arr) {
+    product = product*item;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(target, power) {
+  let result = target;
+  for (let i = 1; i < power; i ++) {
+    result = result * target;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+
+  let result = 1;
+
+  for (let i = num; i > 0; i --) {
+    result = result * i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = str.split("");
+    arr = arr.filter(str => str !== '!' && str !== " " && str !== "." && str !== ",");
+    str = arr.join("");
+    str = str.toLowerCase();
+    console.log(arr);
+    for (let i = 0; i < str.length/2; i ++) {
+        if (str[i] != str[str.length-1-i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line

8438005175c28276dd7fef837722179378410edf
Ben Hustler <ben1hustler@gmail.com>
2025-12-24 02:20:31 +0000

    Finished more js practices

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    if (arr.length == 0) {
+        return "EMPTY LIST";
+    }
 
+    let oldest = arr[0];
+    let oYearOfDeath = arr[0].yearOfDeath;
+    if (!oYearOfDeath) {
+        oYearOfDeath = 2025;
+    }
+
+    for (object of arr) {
+        let yearOfDeath = object.yearOfDeath; 
+        if (!yearOfDeath) {
+            yearOfDeath = 2025;
+        }
+
+        if (yearOfDeath - object.yearOfBirth > oYearOfDeath - oldest.yearOfBirth) {
+            oldest = object;
+            oYearOfDeath = yearOfDeath;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
